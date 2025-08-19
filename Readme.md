e801019927b091afe60c03eeb89a5d79725ea385
p0syd0n <ajeff9752@gmail.com>
2025-08-19 03:49:24 +0000

    I added the code

---
+++ Readme.md
@@ -0,0 +1,19 @@
+# Hashtable!!
+
+## A dictionary (hashtable) in C. 
+
+### Very easy to use. Its all in example.c . 
+
+To be able to use the functions, just do 
+
+``` #include "hashtable.c"```
+
+At the top of your file. Don't mess up the path.
+
+
+## How do I run the example
+
+``` gcc -o example example.c ```
+
+```./example```
+
--- example.c
+++ example.c
@@ -2,7 +2,7 @@
 #include "src/hashtable.c"
 
 int main() {
-  Hashtable* hashtable = create_hashtable(100000);
+  Hashtable* hashtable = create_hashtable(100000); // This will be enough to contain 100,000 seperate elements. However, it is also 800 kilobytes in ram.
 
   table_add(hashtable, "key", "value");
   table_add(hashtable, "beans", "cheese");
@@ -10,13 +10,16 @@ int main() {
 
   char* value = table_get(hashtable, "key");
   char* value2 = table_get(hashtable, "beans");
+  /*
+  Do not free these. Otherwise, you will be left with dangling pointers. If you want to be able to free them, find the line in hashtable.c in table_get and make it say return strdup(node->value); and not return node->value; .  This will, however, make things slower. 
+  */ 
 
   printf("Value: %s, %s\n", value, value2);
 
   int remove = table_remove(hashtable, "beans");
   char* value3 = table_get(hashtable, "beans");
-  printf("Value after being removed: %s\n", value3);
+  printf("Value after being removed: %s\n", value3); //  This should be (nil) or wtv
 
-  free_hashtable(hashtable);
+  free_hashtable(hashtable); // Pls free it. It wants freedom. I stand in support of hashtableian abolitionism
 }
 
